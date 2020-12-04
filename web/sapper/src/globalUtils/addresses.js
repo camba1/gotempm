50acc5e98899194cf2787a472a9d89dba05f7972
Juan Peredo <jperedo@bolbeck.com>
2020-12-04 15:53:08 -0600

    updated address of micro services to include the namespace

--- web/sapper/src/globalUtils/addresses.js
+++ web/sapper/src/globalUtils/addresses.js
@@ -1,45 +1,53 @@
-export let productAddresses = { get: "product/productSrv/GetProductById",
-    getAll: "product/productSrv/GetProducts",
-    update: "product/productSrv/UpdateProduct",
-    create: "product/productSrv/CreateProduct",
-    delete: "product/productSrv/DeleteProduct",
+// Prefix used on all goTemp service api requests
+export let goTempePrefix = "goTemp.api."
+
+export let productAddresses = {
+    get: `${goTempePrefix}product/productSrv/GetProductById`,
+    getAll: `${goTempePrefix}product/productSrv/GetProducts`,
+    update: `${goTempePrefix}product/productSrv/UpdateProduct`,
+    create: `${goTempePrefix}product/productSrv/CreateProduct`,
+    delete: `${goTempePrefix}product/productSrv/DeleteProduct`,
     new: "/product/new",
     reload: "product/",
     previousPage: "/product",
     searchPreviousPage: "/"}
 
-export let customerAddresses = { get: "customer/customerSrv/GetCustomerById",
-    getAll: "customer/customerSrv/GetCustomers",
-    update: "customer/customerSrv/UpdateCustomer",
-    create: "customer/customerSrv/CreateCustomer",
-    delete: "customer/customerSrv/DeleteCustomer",
+export let customerAddresses = {
+    get: `${goTempePrefix}customer/customerSrv/GetCustomerById`,
+    getAll: `${goTempePrefix}customer/customerSrv/GetCustomers`,
+    update: `${goTempePrefix}customer/customerSrv/UpdateCustomer`,
+    create: `${goTempePrefix}customer/customerSrv/CreateCustomer`,
+    delete: `${goTempePrefix}customer/customerSrv/DeleteCustomer`,
     new: "/customer/new",
     reload: "customer/",
     previousPage: "/customer",
     searchPreviousPage: "/"}
 
-export let userAddresses = { get: "user/userSrv/GetUserById",
-    getAll: "user/userSrv/GetUsers",
-    update: "user/userSrv/UpdateUser",
-    create: "user/userSrv/CreateUser",
-    delete: "user/userSrv/DeleteUser",
+export let userAddresses = {
+    get: `${goTempePrefix}user/userSrv/GetUserById`,
+    getAll: `${goTempePrefix}user/userSrv/GetUsers`,
+    update: `${goTempePrefix}user/userSrv/UpdateUser`,
+    create: `${goTempePrefix}user/userSrv/CreateUser`,
+    delete: `${goTempePrefix}user/userSrv/DeleteUser`,
     new: "/user/new",
     reload: "user/",
     previousPage: "/user",
     searchPreviousPage: "/"}
 
-export let promotionAddresses = { get: "promotion/promotionSrv/GetPromotionById",
-    getAll: "promotion/promotionSrv/GetPromotions",
-    update: "promotion/promotionSrv/UpdatePromotion",
-    create: "promotion/promotionSrv/CreatePromotion",
-    delete: "promotion/promotionSrv/DeletePromotion",
+export let promotionAddresses = {
+    get: `${goTempePrefix}promotion/promotionSrv/GetPromotionById`,
+    getAll: `${goTempePrefix}promotion/promotionSrv/GetPromotions`,
+    update: `${goTempePrefix}promotion/promotionSrv/UpdatePromotion`,
+    create: `${goTempePrefix}promotion/promotionSrv/CreatePromotion`,
+    delete: `${goTempePrefix}promotion/promotionSrv/DeletePromotion`,
     new: "/promotion/new",
     reload: "promotion/",
     previousPage: "/promotion",
     searchPreviousPage: "/"}
 
-export let authAddresses = { auth: "user/userSrv/auth",
-    register: "user/userSrv/CreateUser",
+export let authAddresses = {
+    auth: `${goTempePrefix}user/userSrv/auth`,
+    register: `${goTempePrefix}user/userSrv/CreateUser`,
     loginPage: "/login",
     previousPage: "/"
     }
